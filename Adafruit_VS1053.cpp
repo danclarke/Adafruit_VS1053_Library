7d470c0c0c12c0c2411b3ee45682b744aa0aecc1
Dan Clarke <dan.clarke@outlook.com>
2018-05-09 20:50:48 +0100

    Add ESP32 specific changes to work with interrupt-based playback

--- Adafruit_VS1053.cpp
+++ Adafruit_VS1053.cpp
@@ -33,9 +33,24 @@ SIGNAL(TIMER0_COMPA_vect) {
 
 volatile boolean feedBufferLock = false;
 
+#ifdef ESP32
+SemaphoreHandle_t bufferSemaphore;
+
+static void feedTask(void *param) {
+	while (true) {
+		xSemaphoreTake(bufferSemaphore, portMAX_DELAY);
+		myself->feedBuffer();
+	}
+}
+
+static void IRAM_ATTR feeder(void) {
+	xSemaphoreGiveFromISR(bufferSemaphore, NULL);
+}
+#else
 static void feeder(void) {
   myself->feedBuffer();
 }
+#endif
 
 #define VS1053_CONTROL_SPI_SETTING  SPISettings(250000,  MSBFIRST, SPI_MODE0)
 #define VS1053_DATA_SPI_SETTING     SPISettings(8000000, MSBFIRST, SPI_MODE0)
@@ -76,6 +91,11 @@ boolean Adafruit_VS1053_FilePlayer::useInterrupt(uint8_t type) {
 #endif
   }
   if (type == VS1053_FILEPLAYER_PIN_INT) {
+#if defined(ESP32)
+	bufferSemaphore = xSemaphoreCreateBinary();
+	xTaskCreatePinnedToCore(feedTask, "VS1053Feeder", 1536, NULL, 10, NULL, 0); // Arduino main loop is on core 1, so process on the 'other' core
+#endif
+
     int8_t irq = digitalPinToInterrupt(_dreq);
     //Serial.print("Using IRQ "); Serial.println(irq);
     if (irq == -1) 
--- README.md
+++ README.md
@@ -1,11 +1,3 @@
-  This is a library for the Adafruit VS1053 Codec Breakout
+This is an update of the [Adafruit VS1053 Library](https://github.com/adafruit/Adafruit_VS1053_Library) for ESP32 with Interrupt support
 
-  Designed specifically to work with the Adafruit VS1053 Codec Breakout 
-  ----> https://www.adafruit.com/products/1381
-
-  Adafruit invests time and resources providing this open source code, 
-  please support Adafruit and open-source hardware by purchasing 
-  products from Adafruit!
-
-  Written by Limor Fried/Ladyada for Adafruit Industries.  
-  BSD license, all text above must be included in any redistribution
+It's currently not fully tested!
